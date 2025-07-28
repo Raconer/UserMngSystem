422bfd9942180beb5758649afc7944bfc69b9a27
kimdongho <cha11enge@hanmail.net>
2025-07-28 11:17:26 +0900

    [Setting] Root 모듈 디펜던시 추가 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
+    kotlin("jvm") version "1.9.25" apply false
     kotlin("plugin.spring") version "1.9.25" apply false
     id("org.springframework.boot") version "3.5.4" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
@@ -9,19 +9,20 @@ plugins {
 allprojects {
     group = "com.user.manage"
     version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral() // 필수: Kotlin stdlib 등 다운로드
+    }
 }
 
 subprojects {
-    repositories {
-        mavenCentral()
-    }
-
+    apply(plugin = "java")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 
-    java {
+    extensions.configure<JavaPluginExtension> {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(17))
         }
@@ -32,9 +33,9 @@ subprojects {
     }
 
     dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-reflect")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+        add("implementation", "org.jetbrains.kotlin:kotlin-reflect")
+        add("testImplementation", "org.springframework.boot:spring-boot-starter-test")
+        add("testImplementation", "org.jetbrains.kotlin:kotlin-test-junit5")
+        add("testRuntimeOnly", "org.junit.platform:junit-platform-launcher")
     }
 }
